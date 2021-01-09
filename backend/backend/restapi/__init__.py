6d5e2612ecd51938f327f1669ad0124ee514a307
Igor Souza <igormcsouza@gmail.com>
2021-01-09 13:04:32 -0300

    Complete crud

--- backend/backend/restapi/__init__.py
+++ backend/backend/restapi/__init__.py
@@ -1,13 +1,14 @@
 from flask import Blueprint
 from flask_restful import Api
 
-from .resources import TODOSimpleCRUD
+from .resources import TODOs, ObjectTODO
 
 
 bp = Blueprint(__name__, 'api', url_prefix='/api')
 api = Api(bp)
 
-api.add_resource(TODOSimpleCRUD, '/todo')
+api.add_resource(TODOs, '/todo')
+api.add_resource(ObjectTODO, '/todo/<string:_id>')
 
 def init_app(app):
     app.register_blueprint(bp)
--- backend/backend/restapi/resources/__init__.py
+++ backend/backend/restapi/resources/__init__.py
@@ -1,44 +1,68 @@
 from datetime import datetime
 
-from flask_restful import Resource
+from flask import abort
+from flask_restful import Resource, reqparse
+from bson.objectid import ObjectId
+
+from ...database import DatabaseWrapper
 
 
-class TODOSimpleCRUD(Resource):
+db = DatabaseWrapper('main')
+class TODOs(Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('task', type=str, help='Task Objective', required=True)
+    parser.add_argument('when', type=str, help='Task deadline')
+    parser.add_argument('by', type=str, help='Who is going to do it')
+    parser.add_argument('done', type=bool, help='If task is done or not')
 
     def get(self):
-        return {'todos': [
-            {
-                'task': 'Finish This',
-                'when': datetime.timestamp(datetime.now()),
-                'by': 'me',
-                'done': False
-            },
-            {
-                'task': 'Goto the Supermarket',
-                'when': datetime.timestamp(datetime.now()),
-                'by': 'me',
-                'done': True
-            },
-            {
-                'task': 'Implement from Database',
-                'when': datetime.timestamp(datetime.now()),
-                'by': 'someone',
-                'done': False
-            },
-        ]}, 200
+        try:
+            todos = db.search({})
+        except Exception as e:
+            abort(500, f'Internal Error: {e}')
+
+        return { 'todos': todos }, 200
 
     def post(self):
-        return {'status': 'this should be a post'}, 200
+        args = self.parser.parse_args()
 
-    def update(self):
-        return {
-            'todo': {
-                'task': 'Implement from Database',
-                'when': datetime.timestamp(datetime.now()),
-                'by': 'someone',
-                'done': True
-            },
-        }, 200
+        try:
+            _id = db.insert(args)
+        except Exception as e:
+            abort(500, f'Internal Error: {e}')
 
-    def delete(self):
-        return { 'status': 'deleted! Well, will soon' }, 200
+        return {'msg': 'done', '_id': _id}, 200
+class ObjectTODO(Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('task', type=str, help='Task Objective', required=True)
+    parser.add_argument('when', type=str, help='Task deadline')
+    parser.add_argument('by', type=str, help='Who is going to do it')
+    parser.add_argument('done', type=bool, help='If task is done or not')
+
+    def get(self, _id):
+        try:
+            todo = db.search({'_id': ObjectId(_id)})
+        except Exception as e:
+            abort(500, f'Internal Error: {e}')
+
+        return { 'todo': todo }, 200
+
+    def put(self, _id):
+        args = self.parser.parse_args()
+
+        try:
+            db.update({ '_id': ObjectId(_id) }, args)
+        except Exception as e:
+            abort(500, f'Internal Error: {e}')
+
+        return { 'msg': 'done' }, 200
+
+    def delete(self, _id):
+        try:
+            db.delete({ '_id': ObjectId(_id) })
+        except Exception as e:
+            abort(500, f'Internal Error: {e}')
+
+        return { 'msg': 'done' }, 200
