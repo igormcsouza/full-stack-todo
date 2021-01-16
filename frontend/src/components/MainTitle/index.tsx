d0f8b5b201c3c18a3656ff10ca329714c3128723
Igor Souza <igormcsouza@gmail.com>
2021-01-16 12:15:13 -0300

    Add main title component

---
+++ frontend/src/components/MainTitle/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MainTitle: React.FC<{}> = () => {
+  return (
+    <p style={{ fontSize: "3em", margin: "20px 0px" }}>
+      <span style={{ fontWeight: "bold" }}>Todo</span>List
+    </p>
+  );
+};
+
+export default MainTitle;
