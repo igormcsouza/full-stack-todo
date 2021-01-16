0c8c834ad60f60a7be9a3514b32d577e74ac0590
Igor Souza <igormcsouza@gmail.com>
2021-01-16 12:16:34 -0300

    Model layout on main panel

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,55 @@
 import React from "react";
+import { Divider } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+import TodoInput from "./components/TodoInput";
+import MainTitle from "./components/MainTitle";
+
+const useStyles = makeStyles({
+  root: {
+    background: "linear-gradient(45deg, #49008E 30%, #26004A 90%)",
+    fontFamily: "Fira Code",
+    position: "fixed",
+    padding: 0,
+    margin: 0,
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    justifyContent: "center",
+  },
+  container: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    margin: "30px 0px",
+    height: "600px",
+    width: "400px",
+    background: "white",
+    borderRadius: 16,
+  },
+  divider: {
+    width: 350,
+    margin: 20,
+  },
+});
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className={classes.root}>
+      <div className={classes.container}>
+        <MainTitle />
+        <TodoInput />
+        <Divider
+          className={classes.divider}
+          variant="middle"
+          orientation="horizontal"
+        />
+      </div>
     </div>
   );
 }
